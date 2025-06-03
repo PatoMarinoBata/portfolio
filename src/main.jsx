325967a7a298e7bd15d0cc7e414991d87d8418bd
Patricio Marino <P.marinobata@hotmail.com>
2025-06-03 12:44:08 -0300

    Corrijo import con minúsculas: portfolio.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio.jsx";// o el componente principal que estés usando
+import Portfolio from "./portfolio.jsx"; // o el componente principal que estés usando
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
