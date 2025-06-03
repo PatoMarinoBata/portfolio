211c2d68da2fd0e59e430a964b5d956ffcc5045f
Patricio Marino <P.marinobata@hotmail.com>
2025-06-02 23:34:24 -0300

    Agrego Portfolio.jsx para resolver import

---
+++ src/Portfolio.jsx
@@ -0,0 +1,8 @@
+export default function Portfolio() {
+    return (
+      <div className="text-white bg-black h-screen flex items-center justify-center">
+        <h1 className="text-4xl">Hola, soy Patricio Marino Bata</h1>
+      </div>
+    );
+  }
+  
