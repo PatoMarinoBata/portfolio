fdb1ec60cebcb09ccce2906d85914d0323703919
Patricio Marino <P.marinobata@hotmail.com>
2025-06-04 16:14:19 -0300

    Agrego configuración de Tailwind (tailwind.config.js, postcss.config.js) y index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { LanguageProvider } from "./LanguageContext";
 import Portfolio from "./portfolio.jsx"; // o admin.jsx si estás editando
+import "./index.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
